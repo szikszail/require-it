2708316ef5191da8d89579e2da4375f7befe3b56
szikszail <sziklaszlo@gmail.com>
2026-02-07 18:26:48 +0100

    debug

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -8,7 +8,7 @@ const globalStuff = JSON.parse(
   execSync("npm list -g --depth=0 --json").toString().trim(),
 );
 const globalModules = Object.keys(globalStuff.dependencies);
-console.log({ globalModules });
+console.log({ globalModules, globalStuff });
 const GLOBAL_MODULE = globalModules.includes("npm")
   ? "npm"
   : globalModules.includes("yarn")
